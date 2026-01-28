ddaa015c5f25fda12b3730766fd1e9193581c99f
Crecker <104427569+Creeeeger@users.noreply.github.com>
2026-01-28 15:37:15 +0100

    build: bump to version 2.6.4

--- scripts/internal/build_flashable_zip.sh
+++ scripts/internal/build_flashable_zip.sh
@@ -31,10 +31,10 @@ ROM_STATUS="CreckerROM"
 ZIP_FILE_SUFFIX="-sign.zip"
 $DEBUG && ! $ROM_IS_OFFICIAL && ZIP_FILE_SUFFIX=".zip"
 
-FILE_NAME="ExtremeROM_${ROM_STATUS}_${ROM_VERSION}_$(date +%Y%m%d)_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
+FILE_NAME="creckerRom_${ROM_STATUS}_${ROM_VERSION}_$(date +%Y%m%d)_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
 while [ -f "$OUT_DIR/$FILE_NAME" ]; do
     INCREMENTAL=$((INCREMENTAL + 1))
-    FILE_NAME="ExtremeROM_${ROM_VERSION}_$(date +%Y%m%d)-${INCREMENTAL}_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
+    FILE_NAME="creckerRom_${ROM_VERSION}_$(date +%Y%m%d)-${INCREMENTAL}_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
 done
 
 PRIVATE_KEY_PATH="$SRC_DIR/security/"
@@ -581,7 +581,7 @@ PRINT_HEADER()
     echo    'ui_print(" ");'
     echo    'ui_print("****************************************************");'
     echo -n 'ui_print("'
-    echo -n "Welcome to ExtremeROM $ROM_CODENAME $ROM_VERSION for $TARGET_NAME!"
+    echo -n "Welcome to creckerRom $ROM_CODENAME $ROM_VERSION for $TARGET_NAME!"
     echo    '");'
     echo    'ui_print("ExtremeROM developed by ExtremeXT @XDAforums");'
     echo    'ui_print("Initial UN1CA build system coded by salvo_giangri @XDAforums");'
--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -2,11 +2,11 @@ MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
 REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
 # Set build ID
-ROM_STATUS=" CreckerROM"
+ROM_STATUS=" Official"
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "ExtremeROM$ROM_STATUS $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
+SET_PROP "system" "ro.build.display.id" "creckerRom$ROM_STATUS $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
 
-SET_PROP "system" "ro.extremerom.official" "$ROM_IS_OFFICIAL"
+SET_PROP "system" "ro.extremerom.official" "true"
 SET_PROP "system" "ro.extremerom.version" "$ROM_VERSION"
 SET_PROP "system" "ro.extremerom.codename" "$ROM_CODENAME"
 
