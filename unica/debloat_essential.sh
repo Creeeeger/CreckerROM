3c1af7a771144889a1c4a6921cc7e964fa09430c
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2026-02-23 13:18:17 +0100

    debloat: add 3 levels of debloat
    This should ensure to remove features hardware-wise not existant on the phone

---
+++ unica/debloat_essential.sh
@@ -0,0 +1,89 @@
+#
+# Copyright (C) 2026
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Essential debloat list (always applied, including when ROM_DEBLOAT_LEVEL="none")
+# - Keep this list very small and only include removals you always want.
+# - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
+# - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
+# - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
+
+# Samsung Defex policy
+SYSTEM_DEBLOAT+="
+dpolicy_system
+"
+
+VENDOR_DEBLOAT+="
+etc/dpolicy
+"
+
+# Recovery restoration script
+VENDOR_DEBLOAT+="
+recovery-from-boot.p
+bin/install-recovery.sh
+etc/init/vendor_flash_recovery.rc
+"
+
+# Samsung SIM Unlock
+SYSTEM_DEBLOAT+="
+system/bin/ssud
+system/etc/init/ssu_r12sxxx.rc
+system/etc/init/ssu.rc
+system/etc/permissions/privapp-permissions-com.samsung.ssu.xml
+system/etc/sysconfig/samsungsimunlock.xml
+system/lib64/android.security.securekeygeneration-ndk.so
+system/lib64/vendor.samsung.hardware.security.ssu-V1-ndk.so
+system/lib64/libssu_keystore2.so
+system/priv-app/SsuService
+"
+
+# eSIM
+if $SOURCE_IS_ESIM_SUPPORTED; then
+    if ! $TARGET_IS_ESIM_SUPPORTED; then
+        SYSTEM_DEBLOAT+="
+        system/etc/permissions/privapp-permissions-com.samsung.android.app.esimkeystring.xml
+        system/etc/permissions/privapp-permissions-com.samsung.euicc.mep.xml
+        system/etc/sysconfig/preinstalled-packages-com.samsung.android.app.esimkeystring.xml
+        system/etc/sysconfig/preinstalled-packages-com.samsung.euicc.xml
+        system/priv-app/EsimKeyString
+        system/priv-app/EsimClient
+        system/priv-app/EuiccService
+        "
+    fi
+fi
+
+
+# Wi-Fi Hotspot Overlays
+PRODUCT_DEBLOAT+="
+overlay/SoftapOverlay6GHz
+overlay/SoftapOverlayDualAp
+overlay/SoftapOverlayOWE
+"
+
+# StorageShare (kSMBd)
+SYSTEM_DEBLOAT+="
+system/bin/ksmbd.addshare
+system/bin/ksmbd.adduser
+system/bin/ksmbd.control
+system/bin/ksmbd.mountd
+system/bin/ksmbd.tools
+system/etc/default-permissions/default-permissions-com.samsung.android.hwresourceshare.storage.xml
+system/etc/init/ksmbd.rc
+system/etc/permissions/privapp-permissions-com.samsung.android.hwresourceshare.storage.xml
+system/etc/sysconfig/preinstalled-packages-com.samsung.android.hwresourceshare.storage.xml
+system/etc/ksmbd.conf
+system/priv-app/StorageShare
+"
