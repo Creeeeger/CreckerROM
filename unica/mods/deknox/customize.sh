f64683f2a1e6bc90a639fccac1b82b263e3f5b97
Crecker <104427569+Creeeeger@users.noreply.github.com>
2026-04-01 19:17:01 +0200

    re-knox: change to the original server to allow wallet to start
    still some keybox issues

--- unica/mods/deknox/customize.sh
+++ unica/mods/deknox/customize.sh
@@ -1,9 +1,6 @@
 # A26 VOLD/MDF blobs
 ADD_TO_WORK_DIR "a26xxx" "system" "."
 
-# Tab S10 FE android_server blobs
-ADD_TO_WORK_DIR "gts10fewifixx" "system" "."
-
 # S25 AIDL ICCC blobs
 ADD_TO_WORK_DIR "pa3qzcx" "system" "system/lib64/aidl_comm_iccc_client.so"
 ADD_TO_WORK_DIR "pa3qzcx" "system" "system/lib64/vendor.samsung.hardware.tlc.iccc-V3-ndk.so"
@@ -11,25 +8,12 @@ ADD_TO_WORK_DIR "pa3qzcx" "system" "system/lib64/vendor.samsung.hardware.tlc.icc
 # DualDAR
 DELETE_FROM_WORK_DIR "system" "system/bin/dualdard"
 DELETE_FROM_WORK_DIR "system" "system/etc/init/dualdard.rc"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libdualdar.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/aidl_comm_ddar_client.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.tlc.ddar-V1-ndk.so"
 
 # eSE COS
 DELETE_FROM_WORK_DIR "system" "system/bin/sem_daemon"
 DELETE_FROM_WORK_DIR "system" "system/etc/init/sem_early.rc"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.sem.factoryapp.xml"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_sem.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_semAidl.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_semRil.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libsec_semTlc.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libspictrl.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.sem-V1-ndk.so"
 DELETE_FROM_WORK_DIR "system" "system/priv-app/SEMFactoryApp"
-
-# Weaver
-DELETE_FROM_WORK_DIR "system" "system/lib64/libhermes_cred.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/android.hardware.weaver-V2-ndk.so"
 
 # HDM
 DELETE_FROM_WORK_DIR "system" "system/priv-app/HdmApk"
@@ -37,8 +21,6 @@ DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.sa
 
 # WSM
 DELETE_FROM_WORK_DIR "system" "system/etc/public.libraries-wsm.samsung.txt"
-DELETE_FROM_WORK_DIR "system" "system/lib64/libhal.wsm.samsung.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.wsm.service-V1-ndk.so"
 
 # Knox ZeroTrust
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/privapp-permissions-com.samsung.android.knox.zt.framework.xml"
@@ -54,4 +36,3 @@ DELETE_FROM_WORK_DIR "system" "system/framework/FabricCryptoLib.jar"
 DELETE_FROM_WORK_DIR "system" "system/lib64/com.samsung.security.fabric.cryptod-V1-cpp.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.fkeymaster-V1-cpp.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.fkeymaster-V1-ndk.so"
-DELETE_FROM_WORK_DIR "system" "system/priv-app/KmxService"
